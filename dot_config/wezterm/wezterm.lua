e8fe7b3544e24d6c6b4e646e5e1a3b41dd2c7ea2
Taiga2022 <f2021212010@gmail.com>
2026-07-26 20:56:56 +0900

    fix:font_size

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 config.automatically_reload_config = true
-config.font_size = 12.0
+config.font_size = 14.0
 config.use_ime = true
 config.window_background_opacity = 0.85
 config.win32_system_backdrop = "Acrylic"
